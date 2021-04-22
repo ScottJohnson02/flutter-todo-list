b7a6d43b4b20565974d258db37381e4048fbaed1
Thom Mondeaux <Thommond554@gmail.com>
2021-04-22 11:42:53 -0400

    created basic files for login/register.

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp =Firebase.initializeApp();
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-        future: _fbApp,
-        builder: (context, snapshot){
-          if(snapshot.hasError){
-            print('You have an error! ${snapshot.error.toString()}');
-            return Text("Something went Wrong!");
-          }else if (snapshot.hasData){
-            return MyHomePage(title:'My Amazing Counter App!');
-          }else{
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-
-          }
-        },
-      )
-      //MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'home_screen': (context) => HomeScreen()
+      },
     );
   }
 }
