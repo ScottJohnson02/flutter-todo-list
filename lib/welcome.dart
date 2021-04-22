f06274b8ca4115dc9a5164971cf7c27111a406ce
Thom Mondeaux <Thommond554@gmail.com>
2021-04-22 11:59:50 -0400

    added basic welcome screen / logo

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -10,22 +10,31 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black,
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
+                Container(
+                    width: 550,
+                    height: 100,
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: Container(
+                        alignment: Alignment.center,
+                        child: Text('Welcome to Todo List!',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold)))),
                 RoundedButton(
-                  colour: Colors.lightBlueAccent,
+                  colour: Colors.green,
                   title: 'Log In',
                   onPressed: () {
                     Navigator.pushNamed(context, 'login_screen');
                   },
                 ),
                 RoundedButton(
-                    colour: Colors.blueAccent,
+                    colour: Colors.grey,
                     title: 'Register',
                     onPressed: () {
                       Navigator.pushNamed(context, 'registration_screen');
