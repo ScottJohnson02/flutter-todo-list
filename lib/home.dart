5bdbe5e97c9780bade0b83187f228bc5abff1db6
Thom Mondeaux <Thommond554@gmail.com>
2021-04-29 13:40:05 -0400

    tasks can be created unique to users.

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+final db = FirebaseFirestore.instance;
 User loggedinUser;
 String loggedinUserEmail;
+String task;
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  hintStyle: TextStyle(color: Colors.white),
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightGreenAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightGreenAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
 
 class Home extends StatefulWidget {
   @override
@@ -46,13 +65,55 @@ class _HomeState extends State<Home> {
               }),
         ],
         title: Text('Home Page'),
-        backgroundColor: Colors.lightBlueAccent,
+        backgroundColor: Colors.green,
       ),
       body: Center(
-        child: Text(
-          "Welcome $loggedinUserEmail",
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
+        child: Container(
+            width: 500,
+            height: 1000,
+            color: Colors.black,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome $loggedinUserEmail",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      task = value;
+                    },
+                    style: TextStyle(color: Colors.white),
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your Task')),
+                GestureDetector(
+                    onTap: () {
+                      db
+                          .collection('users')
+                          .doc(loggedinUser.uid)
+                          .collection('tasks')
+                          .doc(task)
+                          .set({'title': task, 'time': new DateTime.now()});
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.green),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0))),
+                        alignment: Alignment.center,
+                        width: 200,
+                        height: 50,
+                        child: Text('Add New Task',
+                            style: TextStyle(color: Colors.white))))
+              ],
+            )),
       ),
     );
   }
