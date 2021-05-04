3a1194edbbf816ebf846ffa417987ec9a173f259
Scott Johnson <scottyjohnson002@gmail.com>
2021-05-04 14:41:35 -0400

    test code for single page integration

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:uuid/uuid.dart';
+var uuid = Uuid();
 final db = FirebaseFirestore.instance;
 User loggedinUser;
 String loggedinUserEmail;
@@ -23,14 +24,26 @@ const kTextFieldDecoration = InputDecoration(
   ),
 );
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
+
   @override
-  _HomeState createState() => _HomeState();
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Todo List',
+        home: new TodoList()
+    );
+  }
 }
 
-class _HomeState extends State<Home> {
-  final _auth = FirebaseAuth.instance;
+class TodoList extends StatefulWidget {
 
+  @override
+  createState() => new TodoListState();
+
+}
+
+class TodoListState extends State<TodoList> {
+  final _auth = FirebaseAuth.instance;
   void initState() {
     super.initState();
     getCurrentUser();
@@ -49,72 +62,111 @@ class _HomeState extends State<Home> {
     }
   }
 
+  List<String> _todoItems = [];
+  void _addTodoItem(String task) {
+
+    if(task.length > 0) {
+
+      setState(() => _todoItems.add(task));
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+              title: new Text('Mark "${_todoItems[index]}" as done?'),
+              actions: <Widget>[
+                new FlatButton(
+                    child: new Text('CANCEL'),
+
+                    onPressed: () => Navigator.of(context).pop()
+                ),
+                new FlatButton(
+                    child: new Text('MARK AS DONE'),
+                    onPressed: () {
+                        // db
+                        //     .collection('users')
+                        //     .doc(loggedinUser.uid)
+                        //     .collection('tasks').doc(val)
+                        //     .set({'title': val, 'time': new DateTime.now()});
+                      _removeTodoItem(index);
+                      Navigator.of(context).pop();
+                    }
+                )
+              ]
+          );
+        }
+    );
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if(index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index], index);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText, int index) {
+    return new ListTile(
+        title: new Text(todoText),
+        onTap: () => _promptRemoveTodoItem(index)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
-
-                //Implement logout functionality
-              }),
-        ],
-        title: Text('Home Page'),
-        backgroundColor: Colors.green,
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text('Todo List')
       ),
-      body: Center(
-        child: Container(
-            width: 500,
-            height: 1000,
-            color: Colors.black,
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Welcome $loggedinUserEmail",
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                TextField(
-                    obscureText: false,
-                    textAlign: TextAlign.center,
-                    onChanged: (value) {
-                      task = value;
-                    },
-                    style: TextStyle(color: Colors.white),
-                    decoration: kTextFieldDecoration.copyWith(
-                        hintText: 'Enter your Task')),
-                GestureDetector(
-                    onTap: () {
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _pushAddTodoScreen,
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)
+      ),
+    );
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                  appBar: new AppBar(
+                      title: new Text('Add a new task')
+                  ),
+                  body: new TextField(
+                    autofocus: true,
+                    onSubmitted: (val) {
+                      _addTodoItem(val);
+                      Navigator.pop(context);
                       db
                           .collection('users')
                           .doc(loggedinUser.uid)
-                          .collection('tasks')
-                          .doc(task)
-                          .set({'title': task, 'time': new DateTime.now()});
+                          .collection('tasks').doc(val)
+                          .set({'id': uuid.v4(),'title': val, 'time': new DateTime.now()});
                     },
-                    child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(width: 2, color: Colors.green),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(32.0))),
-                        alignment: Alignment.center,
-                        width: 200,
-                        height: 50,
-                        child: Text('Add New Task',
-                            style: TextStyle(color: Colors.white))))
-              ],
-            )),
-      ),
+                    decoration: new InputDecoration(
+                        hintText: 'Enter something to do...',
+                        contentPadding: const EdgeInsets.all(16.0)
+                    ),
+                  )
+              );
+            }
+        )
     );
   }
 }
